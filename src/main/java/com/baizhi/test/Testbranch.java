f50659da1d960fc0f5b4fbe2c4c5cb1cfcef7c40
lingp <1943258707@qq.com>
2018-06-09 14:42:37 +0800

    branch第二次提交

--- src/main/java/com/baizhi/test/Testbranch.java
+++ src/main/java/com/baizhi/test/Testbranch.java
@@ -2,6 +2,6 @@ package com.baizhi.test;
 
 public class Testbranch {
     public static void main(String[] args) {
-        System.out.println("我是否该一个人 ");
+        System.out.println("我爱上一个人  ");
     }
 }
